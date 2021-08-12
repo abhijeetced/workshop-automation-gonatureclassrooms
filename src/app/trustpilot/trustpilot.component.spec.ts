20e99b189c75a5523170e3fed44e99b5f3e5d9fa
BotMyWork <botmywork@Abhijeets-MacBook-Air.local>
2021-08-12 16:22:16 +0530

    update

--- src/app/trustpilot/trustpilot.component.spec.ts
+++ src/app/trustpilot/trustpilot.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { TrustpilotComponent } from './trustpilot.component';
 
@@ -6,7 +6,7 @@ describe('TrustpilotComponent', () => {
   let component: TrustpilotComponent;
   let fixture: ComponentFixture<TrustpilotComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ TrustpilotComponent ]
     })
