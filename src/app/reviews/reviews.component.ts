93d5dd2e7f13efbe4eedb7eeacca674456484761
unknown <abhijeetpratapsingh@cedcoss.com>
2021-08-13 13:19:56 +0530

    thumbnails

--- src/app/reviews/reviews.component.ts
+++ src/app/reviews/reviews.component.ts
@@ -8,42 +8,42 @@ import { DomSanitizer } from '@angular/platform-browser';
 export class ReviewsComponent implements OnInit {
   reviews: any[] = [
     {
-      thumbnail:"https://lorempixel.com/300/300/",
+      thumbnail:"assets/images/thumbnails/i-84Ypj-N4I.png",
       url:"https://www.youtube.com/embed/i-84Ypj-N4I",
       title:"Nidhi Reversed Hypertension & Nose Bleeding (Trailer)"
     },
     {
-      thumbnail:"https://lorempixel.com/300/300/",
+      thumbnail:"assets/images/thumbnails/llOLlRX22Jo.png",
       url:"https://www.youtube.com/embed/llOLlRX22Jo",
       title:"Harpreet Reversed Her 11 Years PCOD Problem (Trailer)"
     },
     {
-      thumbnail:"https://lorempixel.com/300/300/",
+      thumbnail:"assets/images/thumbnails/XU3mtqrex48.png",
       url:"https://www.youtube.com/embed/XU3mtqrex48",
       title:"Kavita Reversed Her 5 Years Old Depression (Trailer)"
     },
     {
-      thumbnail:"https://lorempixel.com/300/300/",
+      thumbnail:"assets/images/thumbnails/M3rBVugrBy8.png",
       url:"https://www.youtube.com/embed/M3rBVugrBy8",
       title:"Varun Bhargava Reversed His 25 Years Old Diabetes (Trailer)"
     },
     {
-      thumbnail:"https://lorempixel.com/300/300/",
+      thumbnail:"assets/images/thumbnails/nlkZPVP-OY0.png",
       url:"https://www.youtube.com/embed/nlkZPVP-OY0",
       title:"Dipak Reversed 9 Years Old Diabetes (Trailer)"
     },
     {
-      thumbnail:"https://lorempixel.com/300/300/",
+      thumbnail:"assets/images/thumbnails/MW2_55Fx5eU.png",
       url:"https://www.youtube.com/embed/MW2_55Fx5eU",
       title:"Satish Kumar Reversed His 20 Years Old Constipation (Trailer)"
     },
     {
-      thumbnail:"https://lorempixel.com/300/300/",
+      thumbnail:"assets/images/thumbnails/Ef4xB5BX-qs.png",
       url:"https://www.youtube.com/embed/Ef4xB5BX-qs",
       title:"Tarun Bhargava Reversed His Hypertension and Lost 27 kg Weight (Trailer)"
     },
     {
-      thumbnail:"https://lorempixel.com/300/300/",
+      thumbnail:"assets/images/thumbnails/_OhE_LDiYI4.png",
       url:"https://www.youtube.com/embed/_OhE_LDiYI4",
       title:"Bhavesh Reversed 8 Years Old Sciatica (Trailer)"
     }
