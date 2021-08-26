ebf7e9c57ddec183e6db4cb7bdb53189dfc919e5
BotMyWork <botmywork@Abhijeets-MacBook-Air.local>
2021-08-26 17:15:06 +0530

    graphics

--- src/app/components/mail-cta/mail-cta.component.ts
+++ src/app/components/mail-cta/mail-cta.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AngularFirestore } from '@angular/fire/firestore';
+window as any;
 @Component({
   selector: 'gonature-workshop-mail-cta',
   templateUrl: './mail-cta.component.html',
   styleUrls: ['./mail-cta.component.css']
 })
 export class MailCtaComponent implements OnInit {
+  mobile:string;
+  waiting:boolean;
+  constructor(private db: AngularFirestore) { }
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  join(mobile:string){
+    this.waiting = true;
+    this.db.collection('mobile_numbers').add({mobile:mobile}).then((v)=>{
+      window.location.href = "https://pages.razorpay.com/gdiet";
+      this.waiting = false;
+    });
+
   }
 
 }
